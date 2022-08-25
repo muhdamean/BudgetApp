7759c037bbbb82d71b1198037c56801f17ca9bc3
Muhdamean <muhdamean@Muhdameans-MacBook-Pro.local>
2022-08-25 19:34:54 +0100

    Initial Files

---
+++ BudgetApp/BudgetApp/View/DashboardPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BudgetApp.View
+{
+    public partial class DashboardPage : ContentPage
+    {
+        public DashboardPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
