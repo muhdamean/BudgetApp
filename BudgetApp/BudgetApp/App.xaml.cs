b0d0e3868cba20a841e94c95e151f571d6022566
Muhdamean <muhdamean@Muhdameans-MacBook-Pro.local>
2022-08-29 22:04:28 +0100

    Dashboard Design 1

--- BudgetApp/BudgetApp/App.xaml.cs
+++ BudgetApp/BudgetApp/App.xaml.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using BudgetApp.View;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+[assembly: ExportFont("Roboto-Regular.ttf", Alias ="NormalFont")]
+[assembly: ExportFont("Roboto-Medium.ttf", Alias = "MediumFont")]
+[assembly: ExportFont("Roboto-Bold.ttf", Alias = "BoldFont")]
+[assembly: ExportFont("Roboto-Light.ttf", Alias = "LightFont")]
 namespace BudgetApp
 {
     public partial class App : Application
@@ -10,7 +15,7 @@ namespace BudgetApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new DashboardPage());
         }
 
         protected override void OnStart ()
