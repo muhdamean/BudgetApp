5332b28b1eec664628600ce9f1b38730b0ff6f57
Muhdamean <muhdamean@Muhdameans-MacBook-Pro.local>
2022-09-02 20:04:38 +0100

    add custom date picker

---
+++ BudgetApp/BudgetApp.Android/Renderers/CustomDatePickerRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.Content;
+using BudgetApp.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(DatePicker), typeof(CustomDatePickerRenderer))]
+namespace BudgetApp.Droid.Renderers
+{
+    public class CustomDatePickerRenderer : DatePickerRenderer
+    {
+        public CustomDatePickerRenderer(Context context) : base (context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+
+            if(Control != null)
+            {
+                Control.Text = Element.Date.ToString("dd MMM yyyy").ToUpper();
+
+                Element.DateSelected += (sender, args) =>
+                {
+                    Control.Text = Element.Date.ToString("dd MMM yyyy").ToUpper();
+                };
+            }
+
+            if(e.OldElement == null)
+            {
+                Control.Background = null;
+            }
+        }
+    }
+}
+
---
+++ BudgetApp/BudgetApp.iOS/Renderers/CustomDatePickerRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using BudgetApp.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(DatePicker), typeof(CustomDatePickerRenderer))]
+namespace BudgetApp.iOS.Renderers
+{
+    public class CustomDatePickerRenderer :DatePickerRenderer
+    {
+        public CustomDatePickerRenderer()
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.Text = Element.Date.ToString("dd MMM yyyy").ToUpper();
+
+                Element.DateSelected += (sender, args) =>
+                {
+                    Control.Text = Element.Date.ToString("dd MMM yyyy").ToUpper();
+                };
+            }
+
+            if (e.OldElement == null)
+            {
+                try
+                {
+                    Control.Background = null;
+                    Control.BorderStyle = UIKit.UITextBorderStyle.None;
+                    Control.Layer.BorderWidth = 0;
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+    }
+}
+
