552f7d3b8d65296af79f978c37064292bd283a03
Muhdamean <muhdamean@Muhdameans-MacBook-Pro.local>
2022-09-14 21:19:23 +0100

    add MessageDialog

--- BudgetApp/BudgetApp.iOS/AppDelegate.cs
+++ BudgetApp/BudgetApp.iOS/AppDelegate.cs
@@ -23,6 +23,7 @@ namespace BudgetApp.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             SQLitePCL.Batteries_V2.Init();
+            Rg.Plugins.Popup.Popup.Init();
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
---
+++ BudgetApp/BudgetApp/View/Popup/MessageDialog.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+
+namespace BudgetApp.View.Popup
+{
+    
+    public partial class MessageDialog : PopupPage
+    {
+        public MessageDialog(string title, string message, string OkText,
+            Action OkAction=null, string CancelText=null, Action CancelAction=null)
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
+
