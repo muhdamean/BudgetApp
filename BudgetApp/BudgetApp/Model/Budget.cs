bd84edb79db56f8f0d5ceb4e6f859a4b8e9429ce
Muhdamean <muhdamean@Muhdameans-MacBook-Pro.local>
2022-09-25 11:10:43 +0100

    update budget list, amountspent and percentage

--- BudgetApp/BudgetApp/Model/Budget.cs
+++ BudgetApp/BudgetApp/Model/Budget.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using Xamarin.Forms;
 
 namespace BudgetApp.Model
 {
@@ -16,6 +18,16 @@ namespace BudgetApp.Model
         public float AmountSpent { get; set; }
         [NotMapped]
         public float Percentage=>  (AmountSpent / Amount) * 100;
+        [NotMapped]
+        public Color Color => GetColor();
+
+        private Color GetColor()
+        {
+            string[] color = { "#FFC44B", "#887DFB", "#21C8ED", "#3F32C7" };
+            var index = new Random().Next(0, color.Count());
+
+            return Color.FromHex(color[index]);
+        }
     }
 
     public enum Duration
