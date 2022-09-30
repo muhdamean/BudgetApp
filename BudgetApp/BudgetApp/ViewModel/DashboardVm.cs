1ac0235e70cd80b59b429f2f7792d8f3ac36f3f4
Muhdamean <muhdamean@Muhdameans-MacBook-Pro.local>
2022-09-30 10:32:09 +0100

    update dashboard transactions

--- BudgetApp/BudgetApp/ViewModel/DashboardVm.cs
+++ BudgetApp/BudgetApp/ViewModel/DashboardVm.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using BudgetApp.Data;
 using BudgetApp.Model;
 using BudgetApp.View;
 
@@ -9,8 +13,11 @@ namespace BudgetApp.ViewModel
     {
         public DashboardVm()
         {
+            service = new DataService();
             MenuList = GetMenu();
+            GetTransactions();
         }
+        private DataService service;
 
         private ObservableCollection<Menu> menuList;
 
@@ -18,6 +25,43 @@ namespace BudgetApp.ViewModel
         {
             get { return menuList; }
             set { menuList = value; }
+        }
+
+        private List<Transaction> transactions;
+        public List<Transaction> Transactions
+        {
+            get { return transactions; }
+            set
+            {
+                transactions = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(CurrentBalance));
+                OnPropertyChanged(nameof(CurrentIncome));
+                OnPropertyChanged(nameof(CurrentExpenses));
+                OnPropertyChanged(nameof(RecenetTransactions));
+            }
+        }
+
+        public float CurrentBalance { get; set; }
+        public float CurrentIncome { get; set; }
+        public float CurrentExpenses { get; set; }
+        public List<Transaction> RecenetTransactions { get; set; }
+
+
+        private async void GetTransactions()
+        {
+            var transactions = await service.GetTransactionsAsync();
+            Transactions = transactions.ToList();
+
+            var startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+            var endDate = startDate.AddMonths(1).AddDays(-1);
+
+            RecenetTransactions = Transactions.Where(x => x.Date >= startDate && x.Date <= endDate).ToList();
+            CurrentIncome = RecenetTransactions.Where(x => x.TransactionType == TransactionType.Income).Sum(x => x.Amount);
+            CurrentExpenses = RecenetTransactions.Where(x => x.TransactionType == TransactionType.Expense).Sum(x => x.Amount);
+
+            CurrentBalance = Transactions.Where(x => x.TransactionType == TransactionType.Income).Sum(x => x.Amount) -
+                            Transactions.Where(x => x.TransactionType == TransactionType.Expense).Sum(x => x.Amount);
         }
 
         private ObservableCollection<Menu> GetMenu()
